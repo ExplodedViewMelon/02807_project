0baa3813a515a8d93d5956d9591890ddd8d44ff4
kledisalla <142668349+kledisalla@users.noreply.github.com>
2023-11-21 21:07:32 +0200

    Update utils.py
    jaccard similarity function added, plus TF-IDF function for the input

--- utils.py
+++ utils.py
@@ -32,3 +32,29 @@ def get_referenced_by(df: pd.DataFrame) -> pd.DataFrame:
 #Filtering
 filtered_df=full_df[(full_df["n_counted_citations"]!=0) & (full_df["n_references"]!=0) &
                     (full_df["abstract"]!="") & (full_df["title"]!="")]
+
+#TF-IDF on the user input + jaccard similarity function 
+from collections import Counter
+
+def input_tf_idf(user_input):
+    tokenize_input=user_input.lower().split()
+    word_count=Counter(tokenize_input)
+    total_words=len(tokenize_input)
+    tf={word:count/total_words for word,count in word_count.items()}    
+    return tf
+
+
+def jaccard_similarity(tf,keywords):
+    input_words=list(tf.keys())
+    union=0
+    intersection=0
+    set_input_words=set(input_words)
+    set_keywords=set(keywords)
+    for keyword in set_input_words:
+        if keyword in set_keywords:
+            intersection=intersection+1
+    unionlist=input_words+keywords
+    unionset=set(unionlist)
+    union=len(unionset)
+    similarity=intersection/union
+    return similarity
