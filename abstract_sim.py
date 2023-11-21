e72a974c8434b3e991c2c2af61eb0808d5297062
Mathias Rahbek-Borre <urokoz@gmail.com>
2023-11-21 17:06:26 +0100

    abstract script is ready for HPC

--- abstract_sim.py
+++ abstract_sim.py
@@ -5,12 +5,9 @@ import pickle
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-from utils import get_referenced_by, filter_df
+from utils import get_referenced_by, filter_df, clean_text
 from dataloader import CitationDataset
 from pandarallel import pandarallel
-
-
-pandarallel.initialize(nb_workers=10, progress_bar=True)
 
 
 def shingle(text: str, shingle_size):
@@ -34,27 +31,76 @@ def get_signature(text: str, shingle_size = 3, sig_len = 5):
     return signature
 
 
-
-def get_model(save_path: str) -> pd.DataFrame:
+def get_model(save_path: str, shingle_size=2, signature_size=250, subset=False) -> pd.DataFrame:
     if os.path.exists(save_path):
         with open(save_path, "rb") as f:
             df = pickle.load(f)
     else:
         dataset = CitationDataset()
-        df = dataset.load_dataframe(subset=True)
-
+        df = dataset.load_dataframe(subset=subset)
+        
         df = get_referenced_by(df)
+        print("Filtering dataframe...")
         df = filter_df(df)
 
-        k=250
-        shingle_size=3
         df["signature"] = df["abstract"].parallel_apply(get_signature, 
                                                         shingle_size=shingle_size, 
-                                                        sig_len=k)
+                                                        sig_len=signature_size)
         with open(save_path, "wb") as f:
             pickle.dump(df, f)
     return df.convert_dtypes(dtype_backend="pyarrow")
 
 
+def jaccard2(signature1, signature2):
+    import numpy as np
+
+    if signature1 == np.nan or signature2 == np.nan:
+        return 0
+
+    signatures_doc1 = np.array(signature1)
+    signatures_doc2 = np.array(signature2)
+    return len(np.intersect1d(signatures_doc1, signatures_doc2)) / len(
+        np.union1d(signatures_doc1, signatures_doc2)
+    )
 
 
+def get_most_similar(df, promt, shingle_size=2, signature_size=250, n_top=10):
+    clean_promt = clean_text(promt)
+    promt_sig = get_signature(clean_promt, 
+                              shingle_size=shingle_size, 
+                              sig_len=signature_size)
+
+    df["sim"] = df["signature"].apply(jaccard2, signature2=promt_sig)
+
+    return df[df["sim"] > 0].sort_values("sim", ascending=False).head(n_top)
+
+
+if __name__ == "__main__":
+    
+    pandarallel.initialize(nb_workers=4, progress_bar=True)
+    
+    shingle_size = 2
+    signature_length = 250
+    
+    model_name = f"model_df_{shingle_size}_{signature_length}.pkl"
+    
+    model_df = get_model(model_name, 
+                         shingle_size=shingle_size, 
+                         signature_size=signature_length, 
+                         subset=True)
+    
+    test_idx = 500
+    prompt = model_df.iloc[test_idx]["abstract"]
+    model_df = model_df.drop([test_idx], axis=0)
+    # for speedup: https://stackoverflow.com/questions/73845259/efficient-cosine-similarity-between-dataframe-rows
+    
+    print()
+    print(f"{prompt=}")
+
+    df_top = get_most_similar(model_df, 
+                              prompt,
+                              shingle_size=shingle_size,
+                              signature_size=signature_length, 
+                              n_top=10)
+    print()
+    print(f"{df_top[['title', 'sim']]=}")
--- utils.py
+++ utils.py
@@ -48,5 +48,5 @@ def filter_df(df: pd.DataFrame) -> pd.DataFrame:
         
     df = df[(df["n_references"] > 0) | (df["n_counted_citations"] > 0)]
     
-    return df
+    return df.reset_index(drop=True)
     
